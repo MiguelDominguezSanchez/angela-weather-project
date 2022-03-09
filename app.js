f31b7f04b2fb9533ae6c93c0e39862f1f5826cf8
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-03-09 18:30:01 +0100

    How to Parse JSON

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ app.get('/', function (req, res) {
 	const url =
 		'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=6b0c55ffbc450cf1b8b5ff53cbd64f6b&units=metric'
 	https.get(url, function (response) {
-		console.log(response)
+		console.log(response.statusCode)
+
+		response.on('data', function (data) {
+			const weatherData = JSON.parse(data)
+			const temp = weatherData.main.temp
+			const weatherDescription = weatherData.weather[0].description
+			console.log(weatherDescription)
+		})
 	})
 	res.send('Server is up and running')
 })
